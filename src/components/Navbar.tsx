f040fe3f059c048601c0cae3952c2208efb4f38e
Reza 1290 <reza.muktasib@gmail.com>
2025-04-23 21:04:42 +0700

    fix: api

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { signOut } from "next-auth/react";
+import LogoutNavButton from "./buttons/LogoutNavButton";
 
 interface NavbarProps {
   activeTab: 'predict' | 'history';
@@ -36,20 +37,7 @@ function Navbar({ activeTab, setActiveTab }: NavbarProps) {
               {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
               <path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9L0 168c0 13.3 10.7 24 24 24l110.1 0c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24l0 104c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65 0-94.1c0-13.3-10.7-24-24-24z"/></svg>
             </button>
-            <button
-               onClick={async() => {"use server" 
-                
-                signOut({redirectTo: '/auth/signin'})}}
-             
-            >
-              <svg className={`w-6 ${
-                activeTab === 'predict'
-                  ? ' fill-slate-900 font-medium'
-                  : 'hover:fill-slate-500 fill-slate-800 '
-              }`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-              {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
-              <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg>
-            </button>
+            <LogoutNavButton activeTab={activeTab}></LogoutNavButton>
           </div>
         </div>
       </div>
